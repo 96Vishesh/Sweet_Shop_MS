6e5819a6c7ec1f5ef6c46ea315fcc32e0e92aa88
Vishesh96 <srivastava.vishesh96@gmail.com>
2025-12-14 17:37:49 +0530

    Application.properties and Main Class modified to adapt to the env config

--- SweetShop-Backend/com.inn.SweetShop/src/main/java/com/inn/SweetShop/SweetShopManagementSystemApplication.java
+++ SweetShop-Backend/com.inn.SweetShop/src/main/java/com/inn/SweetShop/SweetShopManagementSystemApplication.java
@@ -1,5 +1,6 @@
 package com.inn.SweetShop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SweetShopManagementSystemApplication {
 
 	public static void main(String[] args) {
+		// Load .env file BEFORE Spring Boot starts
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
+
 		SpringApplication.run(SweetShopManagementSystemApplication.class, args);
 	}
-
 }
